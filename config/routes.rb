1e5d85e144f81a1f42454b1498e8f2649e240dac
YutaSakamoto <y.sakamo5@gmail.com>
2019-09-09 11:14:16 +0900

    facebook_authentication_order_completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   root 'pages#home'
 
   get '/dashboard', to: 'users#dashboard'
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   post '/users/edit', to: 'users#update'
 
+  put '/orders/:id/complete', to: 'orders#complete'
   resources :gigs do
     member do
       delete :delete_photo
