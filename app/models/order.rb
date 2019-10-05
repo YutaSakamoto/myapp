a3add1c612e2c6ec9406ebde5d8b593d102992c2
YutaSakamoto <y.sakamo5@gmail.com>
2019-10-05 16:26:07 +0900

    review_added

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,8 @@ class Order < ApplicationRecord
   belongs_to :buyer, class_name: "User"
   belongs_to :seller, class_name: "User"
 
+  has_many :reviews
+
   enum status: [:pending, :accepted, :rejected]
   validates :amount, :days, numericality: { only_integer: true, message: "数値のみ入力可能です"}
 end
